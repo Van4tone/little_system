bb42d3961a880bc0cd88eab1ad139267e258031d
Fan Shi <shifan@ecidme.com>
2021-11-19 17:07:38 +0800

    add can read msg deal

--- Function/Can_Fun/can_fun.c
+++ Function/Can_Fun/can_fun.c
@@ -13,9 +13,31 @@ void CanReadMsgDeal()
 {
 	if(can_recv_ctr.flag.end_s == 1)
 	{
-		if(CanCheckRecvMsg(recv_msg_frame.buf,recv_msg_frame.send_frame.len)==0);
+		if(CanCheckRecvMsg(recv_msg_frame.buf,recv_msg_frame.send_frame.len)==0)
+		{
+			MsgResponseFun();
+			can_send_ctr.flag.err_cnt = 0;
+		}
+		else
+		{
+			can_send_ctr.flag.err_cnt++;
+			CanFunctionInit();
+			MsgErrReponse();
+		}
+		can_send_ctr.flag.start_status = 0;
+		can_send_ctr.flag.bus_status = 0;
+		can_send_ctr.times = 0;
+		can_recv_ctr.times = 0;
+		can_recv_ctr.flag.start_s1 = 0;
+		can_recv_ctr.flag.end_s = 0;
+
+		if(can_send_ctr.flag.err_cnt > CYCLE_CNT_MAX)
+		{
+			can_send_ctr.flag.err_cnt = CYCLE_CNT_MAX;
 			
-			
+			// 错误提示码
+			/**************/
+		}
 	}
 }
 
