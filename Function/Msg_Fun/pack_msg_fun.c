fa35f3c7a2277dfb8f36bd12b8276e115214187a
Fan Shi <shifan@ecidme.com>
2021-11-19 11:34:40 +0800

    add packag msg and run and cfig mode msg pack and send

--- Function/Msg_Fun/pack_msg_fun.c
+++ Function/Msg_Fun/pack_msg_fun.c
@@ -27,7 +27,20 @@ void CfigModeMsgPackagePare()
 	}
 }
 
-
+//运行模式下的发送信息包打包
+void RunModeMsgPackagePare()
+{
+	msg_package_manager.run_msg_send_manager->cfg_run_mode = RUN_MODE;
+	msg_package_manager.run_msg_send_manager->sig_doub_mode = run_manager->mode.sig_doub_mode;
+	msg_package_manager.run_msg_send_manager->cmd = NORM_MSG_MODE;
+	msg_package_manager.run_msg_send_manager->volt_switch = 0;
+	msg_package_manager.run_msg_send_manager->cut_mode = run_manager->mode.cut_mode;
+	msg_package_manager.run_msg_send_manager->coag_mode = run_manager->mode.coag_mode;
+	msg_package_manager.run_msg_send_manager->doub_mode = run_manager->mode.doub_mode;
+	msg_package_manager.run_msg_send_manager->cut_val = run_manager->modes_power[run_manager->mode.cut_mode];
+	msg_package_manager.run_msg_send_manager->coag_val = run_manager->modes_power[run_manager->mode.coag_mode];
+	msg_package_manager.run_msg_send_manager->doub_val = run_manager->modes_power[run_manager->mode.doub_mode];
+}
 
 void PackMsg2Send() // 打包信息并发送
 {
@@ -36,7 +49,19 @@ void PackMsg2Send() // 打包信息并发送
 		if(cfig_manager->mode.cfg_mode_val >= CFIG_R_MODE)
 			return;
 		CfigModeMsgPackagePare();
-		CanMsgPack2Send();
+		CanMsgPack2Send((u8*)msg_package_manager.cfig_msg_send_manager,sizeof(cfig_mode_msg_type));
+	}
+	else
+	{
+		RunModeMsgPackagePare();
+		CanMsgPack2Send((u8*)msg_package_manager.run_msg_send_manager,sizeof(run_mode_msg_type));
+	}
+
+	if(msg_package_manager.cmd != NORM_MSG_MODE)
+	{
+		msg_package_manager.pre_cmd = msg_package_manager.cmd;
+		msg_package_manager.cmd = NORM_MSG_MODE;
+		
 	}
 }
 
